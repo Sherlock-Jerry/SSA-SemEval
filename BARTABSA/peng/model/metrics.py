746e941425c9e403df4a7b6aa5f4db9ff3486719
Sherlock-Jerry <rraghav5600@gmail.com>
2022-01-21 12:38:25 +0000

    fBART ABSA Eval Fixed (Seems so)

--- BARTABSA/peng/model/metrics.py
+++ BARTABSA/peng/model/metrics.py
@@ -2,6 +2,7 @@ from fastNLP import MetricBase
 from fastNLP.core.metrics import _compute_f_pre_rec
 from collections import Counter
 import os, json
+from tqdm import tqdm
 
 class Seq2SeqSpanMetric(MetricBase):
     def __init__(self, eos_token_id, num_labels, opinion_first=True, data_bundle=None, tokenizer = None, mapping2id = None, dataset = None):
@@ -35,7 +36,7 @@ class Seq2SeqSpanMetric(MetricBase):
         self.sentence_id_list = []
 
     def make_phrase(self, start, end, tokenized_sentence):
-        phrase = tokenized_sentence[start-len(self.mapping2id)-3: end-len(self.mapping2id)-3+1]
+        phrase = tokenized_sentence[start-len(self.mapping2id)-len(self.null_token): end-len(self.mapping2id)-len(self.null_token)+1]
         if phrase == []: return [], []
         sent = self.tokenizer.convert_tokens_to_string(tokenized_sentence)
         phrase = self.tokenizer.convert_tokens_to_string(phrase).strip()
@@ -43,7 +44,7 @@ class Seq2SeqSpanMetric(MetricBase):
 
         start_idx = sent.find(phrase) - len(self.null_token)
         assert(start_idx+len(self.null_token)!=-1)
-        end_idx = start_idx+len(phrase) - len(self.null_token)
+        end_idx = start_idx+len(phrase)
 
         return [phrase], [f'{start_idx}:{end_idx}']
 
@@ -62,68 +63,74 @@ class Seq2SeqSpanMetric(MetricBase):
         req_data_len = len(self.data_bundle.get_dataset(category))
         if var_len == req_data_len:
             print()
-            print('*'*20, 'This is test', '*'*20)
+            print('*'*20, f'This is test with {var_len} {req_data_len}', '*'*20)
 
         elif var_len == len(self.data_bundle.get_dataset('dev')):
             print()
-            print('*'*20, 'This is dev', '*'*20)
             category = 'dev'
             req_data_len = len(self.data_bundle.get_dataset(category))
+            print('*'*20, f'This is dev with {var_len} {req_data_len}', '*'*20)
+
         else:
             print(f'Different Lengths -> Req {req_data_len} \t Len {var_len}')
             return
 
         self.sentence_id_list = self.sentence_id_list[-req_data_len:]
+        assert(len(self.sentence_id_list) == len(pred_spans) == req_data_len)
 
         final_data_bundle = self.data_bundle.get_dataset(category)
         master_preds = []
 
-        for i in range(req_data_len):
+        for i in tqdm(range(req_data_len), desc=category):
             idx = list(final_data_bundle['sent_id']).index(self.sentence_id_list[i])
-            tokenized_sent = self.tokenizer.tokenize(final_data_bundle['text'][idx])
+            tokenized_sent_with_NA = self.tokenizer.tokenize(final_data_bundle['text'][idx])
             opinions = []
+
             for pair in list(set(pred_spans[i])):
                 aspect_s, aspect_e, opinion_s, opinion_e, holder_s, holder_e, polarity, intensity = pair
                 
-                aspect_phrase, aspect_phrase_idx = self.make_phrase(aspect_s, aspect_e, tokenized_sent)
-                opinion_phrase, opinion_phrase_idx = self.make_phrase(opinion_s, opinion_e, tokenized_sent)
-                holder_phrase, holder_phrase_idx = self.make_phrase(holder_s, holder_e, tokenized_sent)
+                aspect_phrase, aspect_phrase_idx = self.make_phrase(aspect_s, aspect_e, tokenized_sent_with_NA)
+                opinion_phrase, opinion_phrase_idx = self.make_phrase(opinion_s, opinion_e, tokenized_sent_with_NA)
+                holder_phrase, holder_phrase_idx = self.make_phrase(holder_s, holder_e, tokenized_sent_with_NA)
 
                 polarity_phrase = self.make_pol_int(polarity)
                 intensity_phrase = self.make_pol_int(intensity)
 
                 if aspect_phrase == aspect_phrase_idx == holder_phrase == holder_phrase_idx == opinion_phrase == opinion_phrase_idx == []:
-                    opinions = []
-                    break
+                    # [['something'], ['2:5']]
+                    # opinions = []
+                    continue
 
-                if (polarity_phrase!='NA') and (intensity_phrase!='NA'):
+                if (polarity_phrase!=self.null_token.strip()) and (intensity_phrase!=self.null_token.strip()):
                     single_set = {
-                        "Source": [aspect_phrase, aspect_phrase_idx],
-                        "Target": [holder_phrase, holder_phrase_idx],
+                        "Source": [holder_phrase, holder_phrase_idx],
+                        "Target": [aspect_phrase, aspect_phrase_idx],
                         "Polar_expression": [opinion_phrase, opinion_phrase_idx],
                         "Polarity": polarity_phrase,
                         "Intensity": intensity_phrase,
                     }
                     opinions.append(single_set)
                 else:
-                    opinions = []
-                    break
-            # print(self.tgt_token_list[i,:])            
+                    # opinions = []
+                    # break
+                    continue
+
             master_preds.append({
-                'sent_id_old':final_data_bundle['sent_id'][i],
+                # 'sent_id_old':final_data_bundle['sent_id'][i],
                 'sent_id': self.sentence_id_list[i],
                 'text':final_data_bundle['text'][idx].replace(self.null_token, ''),
                 'opinions':opinions,
                 'pred_span':str(set(pred_spans[i])),
                 'target_span': str(final_data_bundle['target_span'][idx]),
-                'tgt_tokens': str(self.tgt_token_list[idx])
+                'tgt_tokens': str(self.tgt_token_list[i])
             })
 
         category = '_'.join(category)
-        epoch = len([file for file in os.listdir(self.dataset) if 'preds_{category}' in file]) + 1
+        epoch = len([filename for filename in os.listdir(self.dataset) if 'preds_{category}' in filename]) + 1
         print('Saving to File', f'{self.dataset}/preds_{category}_epoch{epoch}.json')
         with open(f'{self.dataset}/preds_{category}_epoch{epoch}.json','w') as f:
-            json.dump(master_preds, f)
+            # json.dump(master_preds, f)
+            f.write(json.dumps(master_preds, indent=4))
 
     def evaluate(self, target_span, pred, tgt_tokens, sent_id):
         
@@ -252,7 +259,7 @@ class Seq2SeqSpanMetric(MetricBase):
             ts = set([tuple(t) for t in ts])
             ps = set(pairs)
 
-            for val in list(set(list(ps)+list(ps))): self.all_pairs.append(val)
+            for val in list(set(list(ps)+list(ts))): self.all_pairs.append(val)
 
             for p in list(ps):
                 if p in ts:
@@ -273,7 +280,7 @@ class Seq2SeqSpanMetric(MetricBase):
     def get_metric(self, reset=True):
         res = {}
         f, pre, rec = _compute_f_pre_rec(1, self.triple_tp, self.triple_fn, self.triple_fp)
-        print('tp, fp, fn', self.triple_tp, self.triple_fp, self.triple_fn)
+        print('\ntp, fp, fn', self.triple_tp, self.triple_fp, self.triple_fn)
         print('Total', self.triple_tp + self.triple_fp + self.triple_fn, len(self.all_pairs))
         print('f, pre, rec', f, pre, rec)
         # _ = input()
@@ -309,6 +316,7 @@ class Seq2SeqSpanMetric(MetricBase):
             self.ae_sc_tp = 0
             self.ae_sc_fn = 0
             self.make_result(self.pred_spans)
+            self.tgt_token_list = []
             self.pred_spans = []
             self.all_pairs = []
         return res
